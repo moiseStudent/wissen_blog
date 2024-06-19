a68e04429d19a48dd525a0556645895a7368a215
Moises Dev <moises.p.student@gmail.com>
2024-06-19 17:46:40 -0400

    Menu de login

--- server.py
+++ server.py
@@ -7,9 +7,23 @@ app = Flask(__name__)
 def not_fount(error):
     return render_template('404.html', error=error)
 
+### Main path ##
 @app.route('/')
 def index():
     return render_template('index.html')
+
+### Blog ##
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/login')
+def login():
+    return render_template('sign.html')
+
+@app.route('/sign')
+def sign():
+    return render_template('sign.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
