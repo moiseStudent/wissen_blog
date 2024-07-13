6c224c2ab4f359f5669682a77c81ba5e73489e25
Luis Fajardo <luisgatuno555@gmail.com>
2024-07-13 10:54:51 -0400

    checkpoint1

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,27 +1,39 @@
+// todos los elementos
+
 const slides = document.querySelectorAll('.slide');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 
-let currentSlide = 0;
+let currentSlide = 0
 
-function showSlide(n) {
-  slides[currentSlide].classList.remove('active');
-  currentSlide = (n + slides.length) % slides.length;
-  slides[currentSlide].classList.add('active');
+slides[0].classList.add("active")
+
+//* funciones
+function showSlide(n){
+  slides[currentSlide].classList.remove("active")
+  currentSlide = (n + slides.length) % slides.length // limita que solamente pueda tener el valores menores a el maximo de slides positivo
+  slides[currentSlide].classList.add("active")
 }
 
-function nextSlide() {
-  showSlide(currentSlide + 1);
+function resetInterval(){
+  clearInterval(autoplayInterval)
+  autoplayInterval = setInterval(nextSlide, 5000);
+}
+function nextSlide(){
+  showSlide(currentSlide + 1)
+  resetInterval()
 }
 
-function prevSlide() {
-  showSlide(currentSlide - 1);
+function prevSlide(){
+  showSlide(currentSlide - 1)
+  resetInterval()
 }
 
 prevBtn.addEventListener('click', prevSlide);
 nextBtn.addEventListener('click', nextSlide);
 
 let autoplayInterval = setInterval(nextSlide, 5000);
+//* bucle
 
 slides.forEach(slide => {
   slide.addEventListener('mouseover', () => {
@@ -32,3 +44,4 @@ slides.forEach(slide => {
     autoplayInterval = setInterval(nextSlide, 5000);
   });
 });
+
