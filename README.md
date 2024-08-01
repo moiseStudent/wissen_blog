ddbe888348ffe6afbb61603166ae723319e65c1c
moises <moises.p.student@gmail.com>
2024-08-01 16:21:15 -0400

    Nueva extructura para wissen blog, faltan elementos por integrar la extructura puede cambiar a medida que voy editando

---
+++ README.md
@@ -0,0 +1,5 @@
+# Forma de ejecutar la app
+
+flask --app wissen --debug
+
+Escribir mas tarde como inicializar la base de datos
--- wissen/__init__.py
+++ wissen/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-from config import config
+from . config import config
 
-from routes.routes import routes
-from routes.auth import auth
-from routes.dashboard import dashboard
-from routes.articles import articles
+from . routes.routes import routes
+from . routes.auth import auth
+from . routes.dashboard import dashboard
+from . routes.articles import articles
 
 app = Flask(__name__)
 
@@ -16,6 +16,7 @@ app.register_blueprint(articles)
 @app.errorhandler(404)
 def not_fount(error):
     return render_template('404.html', error=error)
+
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
