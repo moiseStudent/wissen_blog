8a8d3f6f99cca90ff0ab729099adc4df1ca2ef69
moises <moises.p.student@gmail.com>
2024-08-02 14:23:56 -0400

    Implementaion del login, faltan elementos

--- wissen/routes/auth.py
+++ wissen/routes/auth.py
@@ -51,7 +51,62 @@ def register():
     
     return render_template('auth/register.html')
 
-
-@bp.route('/login')
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = "Usuario incorrecto."
+        
+        elif not check_password_hash(user['password'], password):
+            error = 'Clave incorrecta.'
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index')) # cambiar nombre
+        
+        flash(error)
+    
     return render_template('auth/login.html')
+
+
+### Cargar informacion del usuario si esta disponible
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
+### Cerrar la session
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index')) # Cmbiar nombre de index
+
+
+### Autenticacion requerida
+def login_required(view):
+    @functools.wraps(view)
+    
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs)
+    
+    return wrapped_view
