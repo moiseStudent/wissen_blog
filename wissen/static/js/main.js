2b128bfdb614b2d4082b820f433a5948aa5ef3e0
moises <moises.p.student@gmail.com>
2024-08-09 18:39:45 -0400

    boton para el dashboard y estilos del mensajes flash de flask

---
+++ wissen/static/js/main.js
@@ -0,0 +1,6 @@
+/* Funcion para cerrar los flashed de flask estilizados con bootstrap */
+function flash_messages(){
+
+    var flash_sms = document.getElementById('flashed-sms');
+    flash_sms.style.display = "None";
+}
