bedce6ead0300d174da7a6b18cb9afe915fd23a1
pinguino <luisgatuno555@gmail.com>
2024-08-09 12:25:08 -0400

    checkpoint 3

--- wissen/static/js/eyePassword.js
+++ wissen/static/js/eyePassword.js
@@ -2,13 +2,20 @@ const eye1 = document.getElementById("eye1");
 const eye2 = document.getElementById("eye2");
 const eyeContainer1 = document.getElementById("eye-container1");
 const eyeContainer2 = document.getElementById("eye-container2");
+const passwordMain = document.getElementById("password-main");
+const passwordVerify = document.getElementById("password-verify");
 
-function eyechange1() {
-    eye1.src = "../static/picture/hide.png";
-}
-function eyechange2() {
-    eye2.src = "../static/picture/hide.png";
-}
-
-addEventListener(eyeContainer1, eyechange1);
-addEventListener(eyeContainer2, eyechange2);
+eye1.addEventListener("click", () =>
+    eye1.src == "http://127.0.0.1:5000/static/picture/hide.png"
+        ? ((eye1.src = "../static/picture/view.png"),
+          (passwordMain.type = "password"))
+        : ((eye1.src = "../static/picture/hide.png"),
+          (passwordMain.type = "text"))
+);
+eye2.addEventListener("click", () =>
+    eye2.src == "http://127.0.0.1:5000/static/picture/hide.png"
+        ? ((eye2.src = "../static/picture/view.png"),
+          (passwordVerify.type = "password"))
+        : ((eye2.src = "../static/picture/hide.png"),
+          (passwordVerify.type = "text"))
+);
