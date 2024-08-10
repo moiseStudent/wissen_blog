cc96eacc350d5d0e8cf618bdaf721a03858b0271
moises <moises.p.student@gmail.com>
2024-08-10 19:31:53 -0400

    Actualizacion del dashboard 1

--- wissen/routes/blog.py
+++ wissen/routes/blog.py
@@ -99,3 +99,9 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
+
+
+### Read post
+@bp.route('/article') # Usar el id del post
+def article():
+    return render_template('reading.html')
--- wissen/schema.sql
+++ wissen/schema.sql
@@ -19,5 +19,6 @@ CREATE TABLE post (
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
+    section TEXT,
     FOREIGN KEY (author_id) REFERENCES user (id)
 );
